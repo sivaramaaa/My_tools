615e5b40d07b960a4a9fd2de999908b4640ccec5
sivaramaaa <sivaramaaa@gmail.com>
2017-01-08 12:37:06 +0530

    Add files via upload

---
+++ Format_String/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='frmstr',
+      version='1.0',
+      description='Blind_Format_string_attack',
+      author='Sivaramaaa',
+      author_email='sivaramaaa@gmail.com',
+      py_modules = ['frmstr'],
+      )
