3a2c4cb155f2dcb85f7ecee2e2b0afa76930a574
sivaramaaa <sivaramaaa@gmail.com>
2017-03-11 03:44:24 +0530

    Update device-plug.sh

--- udev_notify/device-plug.sh
+++ udev_notify/device-plug.sh
@@ -3,4 +3,4 @@
 export DISPLAY=:0
 export XAUTHORITY=/home/siva/.Xauthority
 #zenity --info --text 'USB Inserted'
-notify-send -i /home/siva/Desktop/SIh/pirate-sword-skull1-2-256.png "USB Removed"
+notify-send -i /home/siva/Desktop/SIh/pirate-sword-skull1-2-256.png "USB Inserted"
