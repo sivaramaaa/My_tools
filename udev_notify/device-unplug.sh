97efde658e8193af7594e500bb48c4e4b8914f82
sivaramaaa <sivaramaaa@gmail.com>
2017-03-11 03:28:30 +0530

    Add files via upload

---
+++ udev_notify/device-unplug.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DISPLAY=:0
+export XAUTHORITY=/home/siva/.Xauthority
+#zenity --info --text 'USB Inserted'
+notify-send -i /home/siva/Desktop/SIh/pirate-sword-skull1-2-256.png "USB Removed"
